0d650fa714e8d99f89522c768fad017ae8e8ba04
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-05-05 06:43:16 +0000

    [Add] views-user-edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,8 +4,21 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find_by(account_name: params[:account_name])
   end
+  
+  def update
+    @user = User.find_by(account_name: params[:account_name])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end 
 
   def withdraw
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:account_name, :name, :first_name, :last_name, :email)
   end
 end
