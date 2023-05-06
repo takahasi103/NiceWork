d4b34aa6ecf32a7fdcc3d039677c084cbb7afe8b
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-05-06 08:22:09 +0000

    [Add] favorite-function

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,2 +1,21 @@
 class Public::FavoritesController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
+    favorite.save
+  end
+
+  def cancel
+    @post = Post.find(params[:post_id])
+    favorite = @post.favorites.find_by(user_id: current_user.id)
+    favorite.unlike
+  end
+
+  def apply
+    @post = Post.find(params[:post_id])
+    favorite = @post.favorites.find_by(user_id: current_user.id)
+    favorite.relike
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,16 @@ class Post < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
+  
+  #favoritesテーブルの指定したレコードのis_cancelの値を確認する
+  def is_cancelled?(current_user, post)
+    favorite = Favorite.find_by(user: user, post: post)
+    favorite&.is_cancel || false
+  end 
+  
+  #favoritesテーブルのis_cancelがfalseのいいね数をカウントする
+  def count_valid_likes
+    self.favorites.where(is_cancel: false).count
+  end　
+  
 end
