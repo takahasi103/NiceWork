b6744d8bd7c307ddc3661625959d9d29aef1c2b6
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-05-06 01:47:49 +0000

    [Add] post-edit

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -12,10 +12,25 @@ class Public::PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user)
+  end 
   
   private
   
