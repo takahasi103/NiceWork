cb0869a49ee93c413312a97785cd02b050c3c077
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-05-07 12:14:12 +0000

    [Add] admin-user-edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit]
-  
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -10,11 +10,25 @@ class Admin::UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    @user.update(user_params)
+    redirect_to admin_user_path(@user.id)
+  end 
   
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
   private
-  
+
+  def user_params
+    params.require(:user).permit(:account_name, :name, :first_name, :last_name, :email, :is_deleted)
+  end
+
   def set_user
     @user = User.find(params[:id])
-  end 
-   
+  end
+
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,4 +1,5 @@
 class Public::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def create
     @post = Post.new(post_params)
@@ -12,23 +13,19 @@ class Public::PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
     @user = @post.user
     @comment = Comment.new
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to user_path(current_user)
   end 
@@ -38,5 +35,9 @@ class Public::PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:user_id, :body)
   end
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end 
   
 end
