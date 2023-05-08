2907735b1e3b4050a7446b408279d4353ef75b92
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-05-08 12:37:23 +0000

    [Add] levelup-function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  #現在のユーザーの経験値が次のレベルまでの必要な経験値に対して何％の進捗があるかを計算する
+  def experience_percentage(current_user)
+    level = current_user.level
+    threshold = LevelSetting.find_by(level: level + 1).threshold
+    percentage = (current_user.experience_point.to_f / threshold.to_f) * 100
+    percentage > 100 ? "100%" : "#{percentage.round}%"
+  end
 end
