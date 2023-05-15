d384a0df84956b232aa7ce17b16480a78c832bfd
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-05-15 14:25:22 +0000

    [Save] layout

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,9 +3,10 @@ class Public::UsersController < ApplicationController
 
   def show
     @posts = @user.posts.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
-    @favorites = @user.favorited_posts.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    favorites = Favorite.valid_favorites(@user)
+    favorited_posts = Post.where(id: favorites.pluck(:post_id))
+    @visible_favorite_posts = favorited_posts.visible_to(current_user).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
   end
-
 
   def update
     if @user.update(user_params)
