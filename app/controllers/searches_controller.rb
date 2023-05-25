af748149b6ac39745116a752427db073b3674aaf
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-05-25 15:45:39 +0000

    [Fix] searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,9 +6,9 @@ class SearchesController < ApplicationController
     @word = params[:word]
     @range = params[:f]
     if @range == "post"
-      @posts = Post.looks(params[:word]).visible_to(current_user).order(created_at: :desc).page(params[:page]).per(2)
+      @posts = Post.looks(params[:word]).visible_to(current_user).order(created_at: :desc).page(params[:page]).per(10)
     else
-      @users = User.looks(params[:word]).page(params[:page]).per(2)
+      @users = User.looks(params[:word]).page(params[:page]).per(10)
     end 
   end
   
